1a249ee37384a4b50c5fd991aa514f6a413f56d9
Reyhanhuseynova <reyhanhuseynova118@gmail.com>
2022-11-16 02:00:22 +0400

    Init

---
+++ EduHome/EduHome/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using EduHome.DAL;
+using EduHome.Models;
+using EduHome.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduHome.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public HomeController(AppDbContext db)
+        {
+            _db= db;    
+        }
+        
+
+        public IActionResult Index()
+        {
+           
+            HomeVM homeVM= new HomeVM
+            {
+                Services= _db.Services.ToList(), 
+                Sliders = _db.Sliders.ToList(),
+                About=_db.Abouts.First(),
+                Courses=_db.Courses.ToList(),
+                FeedBack=_db.FeedBacks.First(),
+                NoticeBoards= _db.NoticeBoards.ToList(),    
+            };
+            return View(homeVM);
+        }
+
+      
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ EduHome/EduHome/Migrations/20221115081207_CreateFeedBacksTable.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EduHome.Migrations
+{
+    public partial class CreateFeedBacksTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FeedBacks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Position = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FeedBacks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FeedBacks");
+        }
+    }
+}
